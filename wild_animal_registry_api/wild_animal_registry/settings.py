e7b6cc3b4cd47c72f2dab6b7c578f77aa8dab00d
Tanel <tanel.prikk@hotmail.com>
2017-04-07 01:27:46 +0300

    CORS support

--- wild_animal_registry_api/wild_animal_registry/settings.py
+++ wild_animal_registry_api/wild_animal_registry/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # additional
+    'corsheaders',
     'django.contrib.postgres',
     'rest_framework',
     # custom
@@ -50,12 +51,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'wild_animal_registry.urls'
 
